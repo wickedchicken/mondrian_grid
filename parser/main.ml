46188e854963133efda3ccb96dc7166782093eff
Mike Stipicevic <stipredirect@gmail.com>
2011-07-27 22:18:58 -0700

    working

--- parser/main.ml
+++ parser/main.ml
@@ -23,9 +23,18 @@ let speclist = [
   ("--", Arg.Rest (fun arg -> args := !args @ [arg]), ": stop interpreting options")
 ]
 
-let print_lst lst = match outputmode with
-  | Dump -> print_divset lst
-  | Xhtml -> print_endline (xhtml_string_divset lst)
+let print_lst lst = match !outputmode with
+  | Dump -> begin
+        match lst with
+          | Nil -> print_endline "empty file!"
+          | Horiz(lst) -> print_divset lst
+          | Vert(lst) -> print_divset lst
+  end
+  | Xhtml -> begin
+        match lst with
+          | Nil -> print_endline "empty file!"
+          | _ -> print_endline (xhtml_string_divset lst)
+  end
 
 let main () =
   let collect arg = args := !args @ [arg] in
@@ -33,10 +42,7 @@ let main () =
 	try
 		let lexbuf = Lexing.from_channel stdin in
       try
-        match Mondrian.input Lexer.token lexbuf with
-          | Nil -> print_endline "empty file!"
-          | Horiz(lst) -> print_lst lst
-          | Vert(lst) -> print_lst lst
+        print_lst (Mondrian.input Lexer.token lexbuf)
       with Parsing.Parse_error -> print_endline "parse error!"; flush stdout; exit 1
 	with End_of_file -> exit 0
       
--- parser/xhtml.ml
+++ parser/xhtml.ml
@@ -1,20 +1,20 @@
 open Div
 
-let divhdr id style = "<div id=\"" ^ id "\" style=\"" ^ style ^ "\">"
+let divhdr id style = "<div id=\"" ^ id ^ "\" style=\"" ^ style ^ "\">\n"
 let sizetostr size = match size with
   | Free -> ""
   | Size(fl,str) -> "size:" ^ (string_of_float fl) ^ str ^ ";"
 
 let style sz = "position:relative;float:left;" ^ (sizetostr sz)
 
-let rec xhtml_string_div div styleoptz = match div with (id,sz,_,divset) -> begin
+let rec xhtml_string_div styleoptz div = match div with (id,sz,_,ds) -> begin
     let divhdr = divhdr id ((style sz) ^ styleoptz) in
-    let body = "<p>" ^ id "</p>" in
-    let morechunx = xhtml_string_divset divset in
+    let body = "<p>" ^ id ^ "</p>" in
+    let morechunx = xhtml_string_divset ds in
     let tail = "</div>\n" in
     divhdr ^ body ^ morechunx ^ tail
   end
-and xhtml_string_divset divset = match divset with
+and xhtml_string_divset ds = match ds with
   | Nil -> ""
-  | Vert(divs) -> let content = List.map xhtml_string_div divs "clear:left;"
-  | Horiz(divs) -> let content = List.map xhtml_string_div divs ""
+  | Vert(divs) -> String.concat "" (List.map (xhtml_string_div "clear:left;") divs)
+  | Horiz(divs) -> String.concat "" (List.map (xhtml_string_div "") divs)
